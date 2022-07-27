d3b3e42c263f1833684a3ca87ecd836ec9112272
McMackin <McMackin98@gmail.com>
2022-07-27 15:03:19 -0700

    Added pages

---
+++ showElement.js
@@ -0,0 +1,10 @@
+function showElement(elementId) {
+    const x = document.getElementById(elementId);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+console.log("pass");
---
+++ twitch.js
@@ -0,0 +1,7 @@
+new Twitch.Embed("twitch-embed", {
+    width: 1200,
+    height: 700,
+    channel: "official_escl",
+    theme: "dark",
+    parent: ["bigmackin.github.io"]
+});
